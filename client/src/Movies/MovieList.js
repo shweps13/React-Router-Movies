d38b10a79036837ab9d0ba660cca8c3be0d96912
Heorhii Siburov <g.siburov@gmail.com>
2019-08-20 13:15:25 -0700

    links from MovieDetails is works

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -33,7 +33,7 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
+      <Link to={`/movies/${movie.id}`}><h2>{title}</h2></Link>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
